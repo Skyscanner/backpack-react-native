ed69c11cc68023e8bd01006bbef174015cd09511
Shaun Donnelly <shaundonnelly@gmail.com>
2018-12-13 16:10:25 +0000

    [No JIRA] Removed web code

--- native/storybook/CenterDecorator.js
+++ native/storybook/CenterDecorator.js
@@ -21,7 +21,7 @@ import {
   spacingBase,
   spacingLg,
   spacingXxl,
-} from '../../packages/bpk-tokens/tokens/base.react.native';
+} from 'bpk-tokens/tokens/base.react.native';
 
 const styles = StyleSheet.create({
   centered: {
