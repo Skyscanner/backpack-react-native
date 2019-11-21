76b61e1a6277fa713d0a9d32f0d86f5bf903f9d9
Tiago Henrique Engel <tiagohngl@gmail.com>
2019-11-21 15:46:21 +0000

    [NO-JIRA] Integrate documentation.js (#289)

---
+++ scripts/npm/documentation.js
@@ -0,0 +1,42 @@
+/*
+ * Backpack - Skyscanner's Design System
+ *
+ * Copyright 2016-2019 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* eslint-disable no-console */
+
+const { execSync } = require('child_process');
+
+const { get } = require('lodash');
+const colors = require('colors');
+
+const pkg = require('../../package.json');
+
+const packagesToRun = get(pkg, ['documentation', 'packages']) || [];
+
+if (packagesToRun.length === 0) {
+  process.exit(0);
+}
+
+console.log('Generating API docs...\n');
+
+const lernaScope = packagesToRun.map(pkgName => `--scope ${pkgName}`).join(' ');
+
+execSync(
+  `npx lerna exec ${lernaScope} -- npx documentation readme index.js --section=API`,
+);
+
+console.log(colors.green('\nAll good!\n'));
