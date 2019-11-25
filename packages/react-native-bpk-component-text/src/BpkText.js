814ef36c9a9ae83b5ee5a7f9b218a630ba2c3768
Mark Murtagh <mark.murtagh@skyscanner.net>
2019-11-25 15:19:53 +0000

    Disabling font scaling in profile when system font size is increased

--- packages/react-native-bpk-component-text/src/BpkText.js
+++ packages/react-native-bpk-component-text/src/BpkText.js
@@ -218,7 +218,7 @@ const BpkText = (props: Props) => {
 
   return (
     // eslint-disable-next-line backpack/use-components
-    <Text style={style} maxFontSizeMultiplier={3.2} {...rest}>
+    <Text allowFontScaing={false} style={style} maxFontSizeMultiplier={3.2} {...rest}>
       {children}
     </Text>
   );
