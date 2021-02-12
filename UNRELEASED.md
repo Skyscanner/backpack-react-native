c71885d95f03e4cf4b9c2249c4409c430554bf47
Ollie Curtis <8831547+olliecurtis@users.noreply.github.com>
2021-02-12 14:40:04 +0000

    [BPK-4392]: Migrate dark mode (#728)
    * [BPK-4392]: Migrate dark mode
    
    * Fix auto formatting
    
    * [BPK-4392]: Removing duplicate test

--- UNRELEASED.md
+++ UNRELEASED.md
@@ -2,6 +2,9 @@
 
 > Place your changes below this line.
 
+**Changed:**
+ - Updated the underlying dark mode library to the native React Native implementation instead of a 3rd party library, for compatibility with RN 0.64.0.
+
 ## How to write a good changelog entry
 
 1. Add 'Breaking', 'Added' or 'Fixed' in bold depending on if the change will be major, minor or patch according to [semver](semver.org).
--- android/app/src/main/java/net/skyscanner/backpack/MainActivity.kt
+++ android/app/src/main/java/net/skyscanner/backpack/MainActivity.kt
@@ -1,5 +1,6 @@
 package net.skyscanner.backpack
 
+import android.content.res.Configuration
 import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.modules.i18nmanager.I18nUtil
@@ -11,6 +12,18 @@ class MainActivity : ReactActivity() {
         BpkTheme.applyDefaultsToContext(this)
         val sharedI18nUtilInstance = I18nUtil.getInstance()
         sharedI18nUtilInstance.allowRTL(this, true)
+    }
+
+  /**
+   * This function has been added due to the following issue in react-native,
+   * whereby the native Appearance implementation does not change the app when changing
+   * from Dark Mode or Light Mode.
+   * https://github.com/facebook/react-native/issues/28823
+   * PR open since 2020: https://github.com/facebook/react-native/pull/29106
+   */
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        getReactInstanceManager().onConfigurationChanged(this, newConfig)
     }
 
     /**
